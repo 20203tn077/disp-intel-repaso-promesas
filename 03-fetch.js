e8dda957ddfbf50c5457179b6eab7df2edb1d720
Rebecca C <20203tn077@utez.edu.mx>
2023-05-09 22:37:59 -0600

    Adición de comentarios

--- 03-fetch.js
+++ 03-fetch.js
@@ -1,26 +1,53 @@
+// ELEMENTOS
+
 const userModal = new bootstrap.Modal('#userModal')
 const listContainer = document.getElementById('listContainer')
 const itemContainer = document.getElementById('itemContainer')
 
+// VARIABLES 
+
+// Obteniendo mis templates desde el HTML
 const listItemTemplate = listContainer.innerHTML
 const itemTemplate = itemContainer.innerHTML
 
-const promise = fetch('https://reqres.in/api/users')
+// FUNCIONES INTERNAS
 
-const render = (template, user) => template
-.replaceAll('$id', user.id)
-.replaceAll('$first_name', user.first_name)
-.replaceAll('$last_name', user.last_name)
-.replaceAll('$email', user.email)
-.replaceAll('$avatar', user.avatar)
+// Función para llenar mis templates con los datos de los usuarios
+function render(template, user) {
+    return template
+        .replaceAll('$id', user.id)
+        .replaceAll('$first_name', user.first_name)
+        .replaceAll('$last_name', user.last_name)
+        .replaceAll('$email', user.email)
+        .replaceAll('$avatar', user.avatar)
+}
 
-const showUser = id =>fetch(new URL(id, 'https://reqres.in/api/users/')).then(res => res.json()).then(({data: user}) => {
-    itemContainer.innerHTML = render(itemTemplate, user)
-    userModal.show()
-})
+// FUNCIONES PÚBLICAS
 
-promise.then(res => res.json()).then(({data: users}) => {
-    let content = ''
-    for (const user of users) content += render(listItemTemplate, user)
-    listContainer.innerHTML = content
-})
+// Función que será llamada desde la vista
+// También se puede con Event listeners, pero es un poco más complejo
+function showUser(id) {
+    fetch(`https://reqres.in/api/users/${id}`)                              // 1.- Consultar
+        .then(res => res.json())
+        .then((res) => {
+            itemContainer.innerHTML = render(itemTemplate, res.data)        // 2.- Pintar los datos
+            userModal.show()                                                // 3.- Abrir modal
+        })
+}
+
+// Eso solo lo escribo como función para mejor estructura
+function showList() {
+    fetch('https://reqres.in/api/users')
+        .then(res => res.json())
+        .then((res) => {
+            let content = ''                                // Declarar contenido
+            for (const user of res.data) {
+                content += render(listItemTemplate, user)   // Para cada usuario, agregar sus datos al contenido
+            }
+            listContainer.innerHTML = content               // Pintar contenido del listado completo
+        })
+}
+
+// INICIALIZACIÓN
+
+showList()
