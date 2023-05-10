bca9e6dbe8fbd56eca5b777c1d25602e54223a08
Rebecca C <20203tn077@utez.edu.mx>
2023-05-09 22:40:05 -0600

    Implementación de templates en script en lugar de vista

--- 03-fetch.js
+++ 03-fetch.js
@@ -10,18 +10,6 @@ const itemContainer = document.getElementById('itemContainer')
 const listItemTemplate = listContainer.innerHTML
 const itemTemplate = itemContainer.innerHTML
 
-// FUNCIONES INTERNAS
-
-// Función para llenar mis templates con los datos de los usuarios
-function render(template, user) {
-    return template
-        .replaceAll('$id', user.id)
-        .replaceAll('$first_name', user.first_name)
-        .replaceAll('$last_name', user.last_name)
-        .replaceAll('$email', user.email)
-        .replaceAll('$avatar', user.avatar)
-}
-
 // FUNCIONES PÚBLICAS
 
 // Función que será llamada desde la vista
@@ -30,7 +18,34 @@ function showUser(id) {
     fetch(`https://reqres.in/api/users/${id}`)                              // 1.- Consultar
         .then(res => res.json())
         .then((res) => {
-            itemContainer.innerHTML = render(itemTemplate, res.data)        // 2.- Pintar los datos
+            itemContainer.innerHTML = `
+            <div class="row g-3 align-items-center mb-3">
+                <div class="col-auto">
+                    <img class="avatar" src="${res.data.avatar}" alt="avatar">
+                </div>
+                <div class="col">
+                    <div class="row gap-3">
+                        <div class="col-md-6 col-lg-4">
+                            <label class="text-muted">Nombre</label>
+                            <h6>${res.data.first_name}</h6>
+                        </div>
+                        <div class="col-md-6 col-lg-4">
+                            <label class="text-muted">Apellido</label>
+                            <h6>${res.data.last_name}</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row gap-3">
+                <div class="col-md-6 col-lg-4">
+                    <label class="text-muted">Id</label>
+                    <h6>${res.data.id}</h6>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <label class="text-muted">Correo electrónico</label>
+                    <h6>${res.data.email}</h6>
+                </div>
+            </div>`                                                         // 2.- Pintar los datos
             userModal.show()                                                // 3.- Abrir modal
         })
 }
@@ -42,7 +57,26 @@ function showList() {
         .then((res) => {
             let content = ''                                // Declarar contenido
             for (const user of res.data) {
-                content += render(listItemTemplate, user)   // Para cada usuario, agregar sus datos al contenido
+                content += `
+                <div class="col-md-6 col-xl-4">
+                    <div class="card shadow w-100">
+                        <div class="card-body">
+                            <div class="row g-3">
+                                <div class="col">
+                                    <div class="vstack gap-2">
+                                        <h6>${user.first_name} ${user.last_name}</h6>
+                                        <span class="text-muted">${user.email}</span>
+                                    </div>
+                                </div>
+                                <div class="col-auto">
+                                    <button class="btn btn-primary" onclick="showUser(${user.id})">
+                                        <i class="fas fa-eye"></i>&nbsp;Detalles
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`                                     // Para cada usuario, agregar sus datos al contenido  
             }
             listContainer.innerHTML = content               // Pintar contenido del listado completo
         })
